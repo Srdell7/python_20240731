962ec932a81f64f12d4a98154d6fc19a9c207e9c
Srdell7 <srdell7@gmail.com>
2024-08-21 11:46:42 +0000

    8-1

---
+++ lesson8/lesson8_2.py
@@ -0,0 +1,17 @@
+import tools
+
+while True:
+    try:
+        name=str(input("請輸入姓名"))
+        height=float(input('請輸入身高'))
+        weight=float(input('請輸入體重'))
+        bmi=round(weight/((height*0.01)**2),ndigits=2)
+    except ValueError:
+        print("格式錯誤")
+        continue
+    grade = tools.get_status_message(bmi)
+    print(f'{name}您好\n您的BMI值是:{bmi}\n體重:{grade}')
+    stuff = input("請問是否繼續輸入資料 ('q':離開,任意鍵:繼續)?")
+    if stuff == 'q':
+        break
+print("應用程式結束")
